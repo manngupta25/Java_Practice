cbc455ed9d2e9b521f793f5d5307ca938a973a61
Mann Gupta <guptamann251@gmail.com>
2024-12-05 21:49:26 +0530

    multiplication of three numbers.

--- multi.java
+++ multi.java
@@ -1,3 +1,9 @@
 public class multi {
-    
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+        int c = 2;
+        int multi = a*b*c;
+        System.out.println(multi);
+    }
 }
