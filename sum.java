252280bbfd152c3f1f0b4c21de771c8c347295db
Mann Gupta <guptamann251@gmail.com>
2024-12-05 21:39:37 +0530

    added substract method

--- sum.java
+++ sum.java
@@ -4,6 +4,8 @@ public class sum {
         int a = 5;
         int b = 10;
         int sum = a + b;
+        int sub = a-b;
         System.out.println(sum);
+        System.out.println(sub);
     }
 }
