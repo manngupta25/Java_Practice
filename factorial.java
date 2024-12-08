770c1d971c7724b3b41d604432403c0d418f26df
Mann Gupta <guptamann251@gmail.com>
2024-12-08 23:38:05 +0530

    new files

---
+++ factorial.java
@@ -0,0 +1,17 @@
+public class factorial {
+    static int fact(int n) 
+    { 
+        int res = 1, i; 
+        for (i = 2; i <= n; i++) 
+            res *= i; 
+        return res; 
+    } 
+  
+    // main method 
+    public static void main(String[] args) 
+    { 
+        int num = 5; 
+        System.out.println("Factorial of " + num + " is "
+                           + fact(5)); 
+    } 
+}
