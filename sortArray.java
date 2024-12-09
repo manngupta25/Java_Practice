815133e97d3052055e4078121551af1bccf867e8
Mann Gupta <guptamann251@gmail.com>
2024-12-09 21:16:19 +0530

    new file in which we are sorting two arrays.

---
+++ sortArray.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class sortArray {
+    public static void main(String args[]) {
+
+        int[] a = { 2, -1, 3, 4 };
+
+        char[] b = { 'b', 'a', 'c', 'b' };
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        System.out.print("");
+        for (int n : a) {
+            System.out.print(n + " ");
+        }
+
+        System.out.print("\n");
+        for (char c : b) {
+            System.out.print(c + " ");
+        }
+    }
+}
