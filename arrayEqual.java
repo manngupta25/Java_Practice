491a094c9159151848cc8efcd93cbe0846f23797
Mann Gupta <guptamann251@gmail.com>
2024-12-09 21:19:50 +0530

    In this file we check that the given arrays are equal or not.

---
+++ arrayEqual.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class arrayEqual {
+    public static void main(String[] args) {
+
+        int a[] = {10, 20, 30};
+        int b[] = {10, 20, 30};
+
+        boolean r = Arrays.equals(a, b);
+
+        if (r) {
+            System.out.println("Equal");
+        } else {
+            System.out.println("Not Equal");
+        }
+    }
+}
