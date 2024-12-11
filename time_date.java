54bfcbab893f8aab0935ab7c45de1c3a8e477b0d
Mann Gupta <guptamann251@gmail.com>
2024-12-11 22:25:11 +0530

    program to print day, date and time.

---
+++ time_date.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class time_date {
+    public static void main(String args[]) {
+        Date current_Date = new Date();
+
+        System.out.println(current_Date);
+
+    }
+}
