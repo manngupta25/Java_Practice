4983c035f4a5adb8017ea44bd7cecfabb56bb323
Mann Gupta <guptamann251@gmail.com>
2024-12-23 23:38:57 +0530

    Update div.java

--- div.java
+++ div.java
@@ -7,12 +7,12 @@ public class div {
         int b = 2;
         int div = a / b;
         System.out.println(div);
-
+        int div1 = x/y;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter First Number:");
         int x= scanner.nextInt();
         System.out.println("Enter Another Number:");
         int y = scanner.nextInt();
-        System.out.println("Divide is " + div);
+        System.out.println("Divide is " + div1);
     }
 }
